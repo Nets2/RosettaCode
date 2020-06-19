880659ccd5c245f72063f3b68b393d7eeeabf0b9
Nets2 <geo.bisch@gmail.com>
2020-06-19 14:05:00 +0200

    Adding basis of C# solution for 100 doors

---
+++ 1/100 Doors/dotnet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Coucou cha tu sais que je t'aime ;)");
+        }
+    }
+}
